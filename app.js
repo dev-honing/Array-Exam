b931d455648069d567015ddac3f888f8a543a0f9
Ho Nyeong <dev.honing@gmail.com>
2023-11-10 13:10:19 +0900

    arr.some() 메서드 로직 작성법을 모르곘다.
    - a가 함수가 아니라는 오류가 출력됨
    -> some()의 매개변수 자리는 함수가 들어갈 공간인 것 같다.

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-const studentList = [ // studentList.length: 17
-  "김우진",
-  "김현",
-  "방승희",
-  "변호녕",
-  "소사무엘",
-  "송영준",
-  "신동현",
-  "오승민",
-  "유승민",
-  "윤준현",
-  "이민구",
-  "이유안",
-  "이은정",
-  "정영식",
-  "최성민",
-  "최은철",
-  "홍문기"
-];
+const arr = ["a", "ab", "abc", "abcd", "abcde"];
 
-const pokemonList = [ // pokemonList.length: 4
-  "피카츄",
-  "라이츄",
-  "파이리",
-  "꼬부기"
-]
-
-// Q. 명단에 같은 이름(성씨제외)을 가진 학생이 두 명 있습니다. 이 중 마지막에 있는 학생의 위치를 찾아 출력하는 코드를 작성하세요.
-
-// A. studentList.some()
+const someArr = arr.some("a");
+console.log(someArr);
